0ee5b57a1fc676a117a60bbe3ca7e2853157812e
zuralldoe <quirico@gmail.com>
2016-11-07 16:45:42 -0500

    first commit

---
+++ Movie GenresLab 10/Movies.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie_GenresLab_10
+{
+    public class Movie
+    {
+        private string FilmTitle;
+        private string FilmCategory;
+
+        #region Get/Set
+        public string FilmTitle1
+        {
+            get
+            {
+                return FilmTitle;
+            }
+
+            set
+            {
+                FilmTitle = value;
+            }
+        }
+
+        public string FilmCategory1
+        {
+            get
+            {
+                return FilmCategory;
+            }
+
+            set
+            {
+                FilmCategory = value;
+            }
+        }
+        #endregion
+
+        public Movie(string title, string category)
+        {
+            FilmTitle1 = title;
+            FilmCategory1 = category;
+        }
+
+        public string MatchCategory(string sategory)
+        {
+            return FilmTitle1;
+        }
+
+        public string getTitle ()
+        {
+
+            return FilmTitle;
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Movie GenresLab 10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie_GenresLab_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string title, category;
+            List<Movie> MovieList = new List<Movie>();
+            
+
+            MovieList.Add(new Movie("The Shawshank Redemption", "Drama"));
+            MovieList.Add(new Movie("Inside Man", "Drama"));
+            MovieList.Add(new Movie("Stranger Than Fiction", "Comedy"));
+            MovieList.Add(new Movie("Along Came Polly", "Comedy"));
+            MovieList.Add(new Movie("Miracle", "Sports"));
+            MovieList.Add(new Movie("The Natural", "Sports"));
+            MovieList.Add(new Movie("Dodgeball: an Underdog Story", "Sports"));
+            MovieList.Add(new Movie("The Incredibles", "Animated"));
+            MovieList.Add(new Movie("The Lion King", "Animated"));
+            MovieList.Add(new Movie("A Goofy Movie", "Animated"));
+
+            foreach (Movie N in MovieList)
+            {
+                Console.WriteLine(N.getTitle());
+                
+            }
+        }
+
+    }
+}
